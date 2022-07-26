7c0439847693a292b6f21d18703f33f053b0465f
Justina Mandaro <bautistajustina7@gmail.com>
2022-07-26 12:26:01 -0700

    commit tree e72785b074a8e483bd045057d887fef87bf95964

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let ride = 2300;
+let city = "NYC";
+let tip = 'generous';
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride < 400) {
+    return('This one is on me!');
+  }
+  else if (ride> 400 && ride<2000) {
+    return('That will be twenty bucks.');
+  }
+  else if (ride>2000 && ride<2500) {
+    return('I will gladly take your thirty bucks.');
+  }
+  else if (ride>2500) {
+    return('No can do.')
+  }
 }
+scuberGreetingForFeet(ride)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return(city === "NYC" ?  'Ok, sounds good.' : 'No go.');
 }
+ternaryCheckCity(city)
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+      return('Thank you.');
+      break;
+    default:
+      return('Bye.');
+      break;
+  }
+}
+switchOnCharmFromTip(tip)
